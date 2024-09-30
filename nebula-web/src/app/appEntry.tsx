d34dde3aaf9c7cb8409886da6719753e81a7ead5
Denlich <muchalovych@gmail.com>
2024-09-30 18:49:33 +0200

    add basic styles

--- nebula-web/src/app/appEntry.tsx
+++ nebula-web/src/app/appEntry.tsx
@@ -3,7 +3,9 @@ import '@/shared/lib/domain/isCurrentHostEqualsCore';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+
 import { appRouter } from './router';
+import './styles/base.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
